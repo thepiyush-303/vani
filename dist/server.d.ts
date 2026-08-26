ab78877e6bd352ffedc1a7371af3d73072096d6d
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2026-08-26 17:05:20 +0530

    rm-dist

--- dist/server.d.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import * as http from 'http';
-declare const wss: import("ws").Server<typeof import("ws"), typeof http.IncomingMessage>;
-export { wss };
-//# sourceMappingURL=server.d.ts.map
--- dist/sharedState.d.ts
+++
@@ -1,4 +0,0 @@
-import { SentenceBuffer } from './sentenceBuffer';
-export declare function getActiveSentenceBuffer(): SentenceBuffer | null;
-export declare function setActiveSentenceBuffer(buf: SentenceBuffer | null): void;
-//# sourceMappingURL=sharedState.d.ts.map
